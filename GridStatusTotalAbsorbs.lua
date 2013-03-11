b7613de76273696d3c01b30c74c399bbdaa72300
Bastien Clément <g@ledric.me>
2013-03-11 11:40:05 +0100

    Initial commit

---
+++ GridStatusTotalAbsorbs.lua
@@ -0,0 +1,120 @@
+--[[
+	Copyright (c) 2013 Bastien Clément
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+
+local GridRoster = Grid:GetModule("GridRoster")
+local GridStatus = Grid:GetModule("GridStatus")
+
+GridStatusTotalAbsorbs = GridStatus:NewModule("GridStatusTotalAbsorbs")
+
+GridStatusTotalAbsorbs.defaultDB = {
+	unit_total_absorbs = {
+		color    = { r = 0.7, g = 0.7, b = 1.0, a = 1.0 },
+		text     = "Total Absorbs",
+		enable   = true,
+		priority = 30,
+		range    = false
+	}
+}
+
+GridStatusTotalAbsorbs.menuName = "Total Absorbs"
+GridStatusTotalAbsorbs.options  = false
+
+local totalabsorbs_options = {
+	["Color"] = {
+		type = "color",
+		name = "Color",
+		desc = "Color for the absorbs bar",
+		hasAlpha = true,
+		get = function()
+			local s = GridStatusTotalAbsorbs.db.profile.unit_total_absorbs.color
+			return s.r, s.g, s.b, s.a
+		end,
+		set = function(_, r,g,b,a)
+			local s = GridStatusTotalAbsorbs.db.profile.unit_total_absorbs.color
+			s.r, s.g, s.b, s.a = r, g, b, a
+            GridStatusTotalAbsorbs:UpdateAllUnits()
+		end
+}
+
+local settings
+
+function GridStatusTotalAbsorbs:OnInitialize()
+	self.super.OnInitialize(self)
+	self:RegisterStatus("unit_total_absorbs", "Total Absorbs", totalabsorbs_options, true)
+	settings = self.db.profile.unit_total_absorbs
+end
+
+function GridStatusTotalAbsorbs:OnStatusEnable(status)
+    if status == "unit_total_absorbs" then
+        self:RegisterEvent("UNIT_ABSORB_AMOUNT_CHANGED", "UpdateUnit")
+        self:UpdateAllUnits()
+    end
+end
+
+function GridStatusTotalAbsorbs:OnStatusDisable(status)
+    if status == "unit_total_absorbs" then
+        for guid, unitid in GridRoster:IterateRoster() do
+            self.core:SendStatusLost(guid, "unit_total_absorbs")
+        end
+        self:UnregisterEvent("UNIT_ABSORB_AMOUNT_CHANGED", "UpdateUnit")
+    end
+end
+
+function GridStatusTotalAbsorbs:Reset()
+	self.super.Reset(self)
+	self:UpdateAllUnits()
+end
+
+function GridStatusTotalAbsorbs:UpdateAllUnits()
+	for guid, unitid in GridRoster:IterateRoster() do
+		self:UpdateUnitAbsorbs(unitid)
+	end
+end
+
+function GridStatusTotalAbsorbs:UpdateUnit(_, unitid)
+    local unitGUID = UnitGUID(unitid)
+    self:UpdateUnitAbsorbs(unitid)
+end
+
+function GridStatusTotalAbsorbs:UpdateUnitAbsorbs(unitid)
+	unitid = UnitGUID(unitid)
+
+	local abs, max = UnitGetTotalAbsorbs(unitid), UnitHealthMax(unitid)
+	local priority = settings.priority
+
+	if abs == 0 then
+		priority = 1
+	end
+
+	self.core:SendStatusGained(
+        unitGUID,
+        "unit_total_absorbs",
+        priority,
+        nil,
+		settings.color,
+        (abs > 0 and tostring(abs) or nil),
+        abs,
+        max,
+        nil
+    )
+end
